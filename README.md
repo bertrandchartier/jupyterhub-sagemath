2266e2a8973dece66d9e4a93d19e51e8db35f28f
CHARTIER <bertrand.chartier@ac-grenoble.fr>
2019-04-14 20:45:16 +0200

    V1

---
+++ README.md
@@ -0,0 +1,11 @@
+﻿# JUPYTERHUB & SAGEMATH
+
+#JUPYTERHUB : Installation serveur sur debian 9
+
+install_jupyterhub.sh
+
+
+# SAGEMATH : Installation serveur sur debian 9
+
+install_sagemath.sh
+
---
+++ install_jupyterhub.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#install paquet
+apt-get update
+apt-get upgrade -y
+apt-get install -y curl software-properties-common
+apt-get install -y python3-pip
+pip3 install jupyterhub
+Pip3 install notebook
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+npm install -g configurable-http-proxy
+
+#Deamon jupyterhub
+chmod +x /etc/init.d/jupyterhub
+
+# Create a default config to /etc/jupyterhub/jupyterhub_config.py
+jupyterhub --generate-config -f /etc/jupyterhub/jupyterhub_config.py
+
+# Start jupyterhub
+service jupyterhub start
+
+# Stop jupyterhub
+service jupyterhub stop
+
+# Start jupyterhub on boot
+update-rc.d jupyterhub defaults
+
+# Or use rcconf to manage services http://manpages.ubuntu.com/manpages/natty/man8/rcconf.8.html
+rcconf
